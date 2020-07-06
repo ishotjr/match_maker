438802025d874720d4069471ce7f7a318283d90b
ishotjr <github@ishotjr.com>
2020-07-06 10:30:58 -0400

    :office: :office: Add Organizations table selection and display

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,61 @@
-import {initializeBlock} from '@airtable/blocks/ui';
+import {
+	initializeBlock,
+	useBase,
+	useRecords,
+	useGlobalConfig,
+	expandRecord,
+	TablePickerSynced,
+	TextButton,
+} from '@airtable/blocks/ui';
 import React from 'react';
 
 function MatchMaker() {
-    return <div>MatchMaker</div>;
+	const base = useBase();
+
+	const globalConfig = useGlobalConfig();
+	const tableId = globalConfig.get('selectedTableId');
+
+	const table = base.getTableByIdIfExists(tableId);
+	const records = useRecords(table);
+
+	const organizations = records ? records.map(record => (
+		<Organization key={record.id} record={record} />
+	)) : null;
+
+    return (
+		<div>
+			<TablePickerSynced globalConfigKey="selectedTableId" placeholder="Pick an Organization table" />
+			{organizations}
+		</div>
+	);
+}
+
+//function Organization({record, completedFieldId, onToggle}) {
+function Organization({record}) {
+	const label = record.name || 'Unnamed record';
+
+	return (
+		<div
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'space-between',
+				fontSize: 18,
+				padding: 12,
+				borderBottom: '1px solid #ddd',
+			}}
+		>
+			{label}
+			<TextButton
+				icon="expand"
+				aria-label="Expand record"
+				variant="dark"
+				onClick={() => {
+					expandRecord(record);
+				}}
+			/>
+		</div>
+	);	
 }
 
 initializeBlock(() => <MatchMaker />);
