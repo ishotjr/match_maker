396197f92a7619ba2978d1f863dd25c3d18bd756
ishotjr <github@ishotjr.com>
2020-07-06 09:51:27 -0400

    :octocat: :tada: Initial commit

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+import {initializeBlock} from '@airtable/blocks/ui';
+import React from 'react';
+
+function MatchMaker() {
+    return <div>MatchMaker</div>;
+}
+
+initializeBlock(() => <MatchMaker />);
